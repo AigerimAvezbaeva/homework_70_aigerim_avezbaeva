604599d258e3d6b749a401980a15806a89736c66
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-04-17 23:25:10 +0600

    Применены миграции по обновлениям в модели Issue

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from api.views import IssueView, IssueDetailView, IssueUpdateView, IssueCreateView, IssueDeleteView
+from api.views import IssueView, IssueDetailView, IssueUpdateView, IssueCreateView, IssueDeleteView, ProjectsView, \
+    ProjectDetailView, ProjectUpdateView, ProjectCreateView, ProjectDeleteView
 
 urlpatterns = [
     path('issues/', IssueView.as_view(), name='issues'),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('update_issue/<int:pk>', IssueUpdateView.as_view(), name='update_issue'),
     path('create_issue/', IssueCreateView.as_view(), name='create_issue'),
     path('delete_issue/<int:pk>', IssueDeleteView.as_view(), name='delete_issue'),
+    path('projects/', ProjectsView.as_view(), name='projects'),
+    path('projects/<int:pk>', ProjectDetailView.as_view(), name='project'),
+    path('update_project/<int:pk>', ProjectUpdateView.as_view(), name='update_project'),
+    path('create_project/', ProjectCreateView.as_view(), name='create_project'),
+    path('delete_project/<int:pk>', ProjectDeleteView.as_view(), name='delete_project'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
 
-from api.serializers import IssuesSerializer
-from tracker_app.models import Issue
+from api.serializers import IssuesSerializer, ProjectsSerializer
+from tracker_app.models import Issue, Project
 
 
 class IssueView(APIView):
@@ -65,3 +65,62 @@ class IssueDeleteView(DestroyAPIView):
         issue_id = issue.id
         issue.delete()
         return Response({'issue_pk': issue_id}, status=status.HTTP_204_NO_CONTENT)
+
+
+class ProjectsView(APIView):
+    def get(self, request, *args, **kwargs):
+        objects = Project.objects.all()
+        serializer = ProjectsSerializer(objects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProjectDetailView(RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectsSerializer
+    lookup_field = 'pk'
+
+
+class ProjectUpdateView(UpdateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectsSerializer
+    lookup_field = 'pk'
+
+    def put(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = ProjectsSerializer(instance, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            self.perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        return instance
+
+
+def perform_update(serializer):
+    instance = serializer.save()
+    return instance
+
+
+class ProjectCreateView(CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectsSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ProjectDeleteView(DestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectsSerializer
+    lookup_field = 'pk'
+
+    def delete(self, request, *args, **kwargs):
+        project = self.get_object()
+        project_id = project.id
+        project.delete()
+        return Response({'project_pk': project_id}, status=status.HTTP_204_NO_CONTENT)
